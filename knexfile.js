5c86ac990367537cf606708f627c4098223ba744
calebfreed <cdfreed7@gmail.com>
2023-06-26 19:37:24 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,11 @@ const path = require("path");
 
 require("dotenv").config();
 
+
 const {
   NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
+  DEVELOPMENT_DATABASE_URL ="postgres://luzvxxyh:OQqSamvju3OT5pGeW8pquDJ0gLhNT2OD@mahmud.db.elephantsql.com/luzvxxyh",
+  PRODUCTION_DATABASE_URL="postgres://rltrhswc:q87r9Jnsz1sBg6hUSnHdTgaN9Oh2U7oE@mahmud.db.elephantsql.com/rltrhswc",
 } = process.env;
 
 const URL =
