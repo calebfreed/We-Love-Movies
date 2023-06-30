5e7083f826edcaedfc7c05034682dd34d30dc983
calebfreed <cdfreed7@gmail.com>
2023-06-29 22:37:59 -0700

    updating with comments

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -5,7 +5,7 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 const theatersRouter = require("../theaters/theaters.router");
 const reviewsRouter = require("../reviews/reviews.router");
 
-
+//router for movies. Is able to show specific theators based on movies that are showing at each location
 router
   .use("/:movieId/theaters", controller.movieExists, theatersRouter);
 
--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -2,6 +2,8 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./theaters.service");
 const moviesService = require("../movies/movies.service");
 
+// the only function in the controller
+// take a list of theaters as well as movies and displays out each movie at each theator and what is showing
 async function list(req, res, next) {
   const listTheaters = await service.list();
   const theatersMovies = await Promise.all(
